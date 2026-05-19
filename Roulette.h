d199daf45b6cbbf0c2823d5a99076bd1a708a02e
Curcan Sara <saracurcan06@gmail.com>
2026-05-19 14:19:31 +0300

    Changed functions name in roulette.h

--- Roulette.h
+++ Roulette.h
@@ -15,16 +15,16 @@ private:
     std::vector<number> wheel;
     number spinWheel() const;
     void betType(int choice,double bet, Player& player, const number winningSlot);
-    void number(double bet, Player& player, const number winningSlot);
-    void color(double bet, PLayer& player, const number winningSlot);
-    void evenOdd(double bet,PLayer& player, const number winningSlot);
+    void numberBet(double bet, Player& player, const number winningSlot);
+    void color(double bet, Player& player, const number winningSlot);
+    void evenOdd(double bet,Player& player, const number winningSlot);
 public:
     Roulette();
     Roulette(std::string game, double bet);
     Roulette(const Roulette& obj);
     Roulette& operator=(const Roulette& obj);
     ~Roulette() override {};
-    void Play(Player& player) override;
+    void play(Player& player) override;
     void printRules() const override;
 
 };
